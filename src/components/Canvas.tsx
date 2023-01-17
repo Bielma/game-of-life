61b604f1149ca219f5c0b37aa2a892de1c8a23fe
bielma <abielmalopez@gmail.com>
2023-01-16 23:56:34 -0600

    Canvas:: use CPDrawer to draw

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,17 @@
-import React, { useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import CPDrawer from "../util/CPDrawer";
 
 export default function Canvas() {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      let cpDrawer = new CPDrawer(canvasRef.current);
+      //cpDrawer.drawAxis();
+      cpDrawer.drawPoint(cpDrawer.XC(1), cpDrawer.YC(8), true);
+      cpDrawer.drawPoint(cpDrawer.XC(0), cpDrawer.YC(5), true);
+    }
+  }, []);
   return (
     <canvas
       id="canvas_plane"
