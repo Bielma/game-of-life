f8cdcba61786c8e454d16c34e29c6fc69a12e748
bielma <abielmalopez@gmail.com>
2023-01-18 17:18:40 -0600

    App::  init matrixBoard with random values

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
 import Canvas from "../../components/Canvas";
 
 function App() {
-  const startGame = () => {};
+  const [matrixBoard, setMatrixBoard] = useState<boolean[][]>([]);
+  function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  function startGame() {
+    const matrix: boolean[][] = [];
+    //const matrix2: number[][] = new Array<Array<number>>();
+    const n = 50;
+    for (let i = 0; i < n; i++) {
+      matrix[i] = new Array(n);
+      for (let j = 0; j < n; j++) {
+        matrix[i][j] = getRandomInt(1, 5) == 3 ? true : false;
+      }
+    }
+    console.log(matrix);
+    setMatrixBoard(matrix);
+  }
 
   return (
     <div className="App row">
