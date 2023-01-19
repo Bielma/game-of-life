10128972f7a8b8d43bece9234004d6c594d87f81
bielma <abielmalopez@gmail.com>
2023-01-18 21:30:57 -0600

    App:: add method to validate neighbors, and update de board

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -11,32 +11,61 @@ function App() {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  useEffect(() => {
-    startGame();
-  }, []);
-
-  function startGame() {
+  function randomPopulation() {
     const matrix: boolean[][] = [];
-    //const matrix2: number[][] = new Array<Array<number>>();
-    const n = 50;
+    const n = 10;
     for (let i = 0; i < n; i++) {
       matrix[i] = new Array(n);
       for (let j = 0; j < n; j++) {
-        matrix[i][j] = getRandomInt(1, 5) == 3 ? true : false;
+        matrix[i][j] = getRandomInt(1, 4) == 3 ? true : false;
       }
     }
-    console.log(matrix);
     setMatrixBoard(matrix);
+  }
+  function startGame() {
+    let max = 100;
+    let cont = 0;
+    let newMatrix = matrixBoard;
+    while (cont < max) {
+      for (let i = 0; i < newMatrix.length; i++) {
+        for (let j = 0; j < newMatrix[i].length; j++) {
+          let numNeighbors = 0;
+          if (i - 1 >= 0 && i + 1 < 10 && j - 1 >= 0 && j + 1 < 10) {
+            if (newMatrix[i][j + 1]) numNeighbors += 1;
+            if (newMatrix[i][j - 1]) numNeighbors += 1;
+            if (newMatrix[i - 1][j]) numNeighbors += 1;
+            if (newMatrix[i - 1][j]) numNeighbors += 1;
+            if (newMatrix[i + 1][j - 1]) numNeighbors += 1;
+            if (newMatrix[i + 1][j + 1]) numNeighbors += 1;
+            if (newMatrix[i - 1][j - 1]) numNeighbors += 1;
+            if (newMatrix[i - 1][j + 1]) numNeighbors += 1;
+
+            if (newMatrix[i][j] && (numNeighbors == 1 || numNeighbors > 3)) {
+              newMatrix[i][j] = false;
+            } else if (!newMatrix[i][j] && numNeighbors == 3) {
+              newMatrix[i][j] = true;
+            }
+            //setMatrixBoard(newMatrix);
+          }
+        }
+      }
+      if (cont == 0 || cont == 999) console.log(newMatrix);
+      cont++;
+    }
+    setMatrixBoard(newMatrix);
   }
 
   return (
     <div className="App row">
       <div className="col-md-12">
-        <Canvas />
+        <Canvas matrix={matrixBoard} />
       </div>
 
       <div className="col-md-12 text-center mt-4">
         <Button onClick={startGame}>Start</Button>
+      </div>
+      <div className="col-md-12 text-center mt-2">
+        <Button onClick={randomPopulation}>Init Random population</Button>
       </div>
     </div>
   );
