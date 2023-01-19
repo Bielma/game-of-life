9cdfda9013aa78922cc4af6a10111d918ad649b2
bielma <abielmalopez@gmail.com>
2023-01-19 16:59:17 -0600

    Canvas:: move logic to game of life here

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,30 +1,94 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import CPDrawer from "../util/CPDrawer";
-
+type GameState = "run" | "pause";
 interface Props {
-  matrix: boolean[][];
+  matrixInit: boolean[][];
   toggleCellValue: Function;
+  gameState: GameState;
 }
-export default function Canvas({ matrix, toggleCellValue }: Props) {
+
+function Canvas({ matrixInit, toggleCellValue, gameState }: Props) {
+  const [matrixBoard, setMatrixBoard] = useState<boolean[][]>([]);
   const canvasRef = useRef(null);
   let drawer: CPDrawer;
 
   //const [drawer, setDrawer] = useState<CPDrawer>(null)
 
   useEffect(() => {
+    setMatrixBoard(matrixInit);
     if (canvasRef.current) {
       drawer = new CPDrawer(canvasRef.current);
       drawer.clearCanvas();
       drawer.drawGrid();
-      printBoardState();
+      printBoardState(matrixBoard);
     }
-  }, [matrix]);
+  }, [matrixInit]);
 
-  async function printBoardState() {
+  useEffect(() => {
+    setMatrixBoard(matrixInit);
+    if (canvasRef.current) {
+      if (drawer == null) {
+        drawer = new CPDrawer(canvasRef.current);
+      }
+      if (gameState == "run") {
+        runGameOfLife();
+      }
+      // drawer = new CPDrawer(canvasRef.current);
+      // drawer.clearCanvas();
+      // drawer.drawGrid();
+      //printBoardState();
+    }
+  }, [gameState]);
+
+  const runGameOfLife = async () => {
+    let currentMatrix = matrixBoard;
+    let newMatrix = currentMatrix;
+
+    while (gameState == "run") {
+      drawer.clearCanvas();
+      drawer.drawGrid();
+      for (let i = 0; i < currentMatrix.length; i++) {
+        for (let j = 0; j < currentMatrix[i].length; j++) {
+          if (currentMatrix[i][j]) {
+            drawer.drawCell(i, j);
+            await new Promise((r) => setTimeout(r, 100));
+          }
+          let numNeighbors = 0;
+          if (i - 1 >= 0 && i + 1 < 10 && j - 1 >= 0 && j + 1 < 10) {
+            if (currentMatrix[i][j + 1]) numNeighbors += 1;
+            if (currentMatrix[i][j - 1]) numNeighbors += 1;
+            if (currentMatrix[i - 1][j]) numNeighbors += 1;
+            if (currentMatrix[i - 1][j]) numNeighbors += 1;
+            if (currentMatrix[i + 1][j - 1]) numNeighbors += 1;
+            if (currentMatrix[i + 1][j + 1]) numNeighbors += 1;
+            if (currentMatrix[i - 1][j - 1]) numNeighbors += 1;
+            if (currentMatrix[i - 1][j + 1]) numNeighbors += 1;
+
+            if (
+              currentMatrix[i][j] &&
+              (numNeighbors == 1 || numNeighbors > 3)
+            ) {
+              newMatrix[i][j] = false;
+            } else if (!newMatrix[i][j] && numNeighbors == 3) {
+              newMatrix[i][j] = true;
+            }
+          }
+        }
+      }
+      currentMatrix = newMatrix;
+    }
+    setMatrixBoard(newMatrix);
+  };
+
+  const drawCell = async (x: number, y: number) => {
+    drawer.drawCell(x, y);
+    await new Promise((r) => setTimeout(r, 15));
+  };
+  async function printBoardState(matrix: boolean[][]) {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j]) {
-          drawer.drawPoint(drawer.XC(i), drawer.YC(j), matrix[i][j]);
+          drawer.drawPoint(i, j, matrix[i][j]);
           await new Promise((r) => setTimeout(r, 15));
         }
       }
@@ -43,9 +107,20 @@ export default function Canvas({ matrix, toggleCellValue }: Props) {
         logicalXCoordinate = drawer.XL(physicalXCoordinate),
         logicalYCoordinate = drawer.YL(physicalYCoordinate);
 
+      console.log({ physicalXCoordinate, physicalYCoordinate });
+      console.log({ logicalXCoordinate, logicalYCoordinate });
       toggleCellValue(
         Math.floor(logicalXCoordinate),
         Math.floor(logicalYCoordinate)
+      );
+      // drawer.drawPoint(
+      //   drawer.XL(Math.floor(physicalXCoordinate)),
+      //   Math.floor(physicalYCoordinate),
+      //   true
+      // );
+      drawer.drawCell(
+        Math.floor(physicalXCoordinate),
+        Math.floor(physicalYCoordinate)
       );
     }
   };
@@ -54,10 +129,12 @@ export default function Canvas({ matrix, toggleCellValue }: Props) {
     <canvas
       id="canvasGL"
       onClick={handleClick}
-      //onContextMenu={handleClick}
+      onContextMenu={handleClick}
       width={500}
       height={500}
       ref={canvasRef}
     ></canvas>
   );
 }
+
+export default Canvas;
