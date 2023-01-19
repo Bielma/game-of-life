f05656556841382de97d2bc806cb3e18c7d42392
bielma <abielmalopez@gmail.com>
2023-01-18 22:40:10 -0600

    CPDrawer:: modify XC, YC, YL, XL methods, add drawGrid method, modify drawPoint method

--- src/util/CPDrawer.ts
+++ src/util/CPDrawer.ts
@@ -1,7 +1,5 @@
-const MAX_Y = 15,
-  MAX_X = 15;
-const MIN_Y = -15,
-  MIN_X = -15;
+const MAX_Y = 25,
+  MAX_X = 25;
 
 class CPDrawer {
   private canvas: HTMLCanvasElement;
@@ -17,50 +15,37 @@ class CPDrawer {
 
   // Returns the physical x-coordinate of a logical x-coordinate:
   XC(x: number) {
-    return ((x - MIN_X) / (MAX_X - MIN_X)) * this.widthCanvas;
+    return (x / MAX_X) * this.widthCanvas;
   }
 
   // Returns the physical y-coordinate of a logical y-coordinate:
   YC(y: number) {
-    return (
-      this.heightCanvas - ((y - MIN_Y) * this.heightCanvas) / (MAX_Y - MIN_Y)
-    );
+    return this.heightCanvas - (y * this.heightCanvas) / MAX_Y;
   }
 
   // Returns the logical x-coordinate of a physical x-coordinate:
   XL(x: number) {
-    return (x * (MAX_X - MIN_X)) / this.widthCanvas + MIN_X;
+    return (x * MAX_X) / this.widthCanvas;
   }
 
   // Returns the logical y-coordinate of a physical y-coordinate:
   YL(y: number) {
-    return MAX_Y - (y * (MAX_Y - MIN_Y)) / this.heightCanvas;
+    return MAX_Y - (y * MAX_Y) / this.heightCanvas;
   }
 
-  drawAxis() {
+  drawGrid() {
     const ctx = this.canvas.getContext("2d"),
       cty = this.canvas.getContext("2d");
 
-    if (ctx && cty) {
-      ctx.beginPath();
-      ctx.moveTo(this.widthCanvas / 2, 0);
-      ctx.lineTo(this.widthCanvas / 2, this.heightCanvas);
-      ctx.stroke();
-
-      cty.beginPath();
-      cty.moveTo(0, this.heightCanvas / 2);
-      cty.lineTo(this.widthCanvas, this.heightCanvas / 2);
-      cty.stroke();
-
-      ctx.font = "10px Arial";
-      ctx.fillStyle = "#fffff";
-      for (let i = MIN_X; i < MAX_X; i++) {
-        if (i === 0) continue;
-        ctx.fillText(`${i}`, this.XC(i), this.YC(-0.2));
-        ctx.fillText(`${i}`, this.XC(0.1), this.YC(i));
+    for (let x = 0; x <= this.widthCanvas; x += 20) {
+      for (let y = 0; y <= this.heightCanvas; y += 20) {
+        ctx?.moveTo(x, 0);
+        ctx?.lineTo(x, this.heightCanvas);
+        ctx?.stroke();
+        ctx?.moveTo(0, y);
+        ctx?.lineTo(this.widthCanvas, y);
+        ctx?.stroke();
       }
-      ctx.fillText(`${MAX_X}`, this.widthCanvas - 10, this.YC(-0.2));
-      ctx.fillText(`${MAX_Y}`, this.XC(0.1), 10);
     }
   }
 
@@ -69,7 +54,7 @@ class CPDrawer {
       ctx = this.canvas.getContext("2d");
     if (ctx) {
       ctx.fillStyle = state ? "#00000" : "#FFFFF";
-      ctx.fillRect(x - 2, y - 2, 4, 4); //square
+      ctx.fillRect(x, y, 21, 21); //square
     }
   }
 
