7f2a05805af4fe63106926b8abc51eff03b63259
bielma <abielmalopez@gmail.com>
2023-01-18 19:06:27 -0600

    Canvas:: pass matrix by props, and print it

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,17 +1,33 @@
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import CPDrawer from "../util/CPDrawer";
 
-export default function Canvas() {
+interface Props {
+  matrix: boolean[][];
+}
+export default function Canvas({ matrix }: Props) {
   const canvasRef = useRef(null);
+  let drawer: CPDrawer;
+
+  //const [drawer, setDrawer] = useState<CPDrawer>(null)
 
   useEffect(() => {
     if (canvasRef.current) {
-      let cpDrawer = new CPDrawer(canvasRef.current);
-      //cpDrawer.drawAxis();
-      cpDrawer.drawPoint(cpDrawer.XC(1), cpDrawer.YC(8), true);
-      cpDrawer.drawPoint(cpDrawer.XC(0), cpDrawer.YC(5), true);
+      drawer = new CPDrawer(canvasRef.current);
+      drawer.clearCanvas();
+      printBoardState();
     }
-  }, []);
+  }, [matrix]);
+
+  async function printBoardState() {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j]) {
+          drawer.drawPoint(drawer.XC(i), drawer.YC(j), matrix[i][j]);
+          await new Promise((r) => setTimeout(r, 15));
+        }
+      }
+    }
+  }
   return (
     <canvas
       id="canvas_plane"
